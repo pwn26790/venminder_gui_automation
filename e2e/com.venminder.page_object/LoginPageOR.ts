6c90949a4d8f4e24fea5af9004e42c13d67582d3
pawan3pillar <33279856+pawan3pillar@users.noreply.github.com>
2018-09-27 16:29:07 +0530

    Delete LoginPageOR.ts

--- e2e/com.venminder.page_object/LoginPageOR.ts
+++
@@ -1,8 +0,0 @@
-import { browser, element, by, protractor } from "protractor";
-
-export class LoginPageOR {
-  username = element(by.id("Username"));
-  password = element(by.id("Password"));
-  login_button = element(by.name("button"));
-  error_message = element(by.css("div.validation-summary-errors>ul>li"));
-}
